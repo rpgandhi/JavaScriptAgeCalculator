be1274f3b069c0efa6a5b20af60d6ff6c45221cb
Rakhee Gandhi <rpgandhi@gmail.com>
2017-11-26 18:08:38 -0800

    Create tests for all planet years.

--- js/AgeCalculator.js
+++ js/AgeCalculator.js
@@ -2,6 +2,10 @@ export class Age {
   constructor(input) {
     this.age = input;
     this.ageInSeconds = input * 365 * 24 * 60 * 60;
+    this.mercuryYears = (input / 0.24).toFixed(2);
+    this.venusYears = (input / 0.62).toFixed(2);
+    this.marsYears = (input / 1.88).toFixed(2);
+    this.jupiterYears = (input / 11.86).toFixed(2);
 
   }
 }
--- spec/AgeCalculator-spec.js
+++ spec/AgeCalculator-spec.js
@@ -6,7 +6,36 @@ describe('Age', function() {
   it('should capture inputted age and set it to variable', function(){
     let myAge = new Age(34);
     expect(myAge.age).toEqual(34);
+  })
 
+  it('should convert age in Earth years to age in Earth seconds', function(){
+    let myAge = new Age(34);
+    expect(myAge.ageInSeconds).toEqual(1072224000);
 
-  });
+  })
+
+  it('should convert age in Earth years to age in Mercury years', function(){
+    let myAge = new Age(34);
+    expect(myAge.mercuryYears).toEqual('141.67');
+
+  })
+
+  it('should convert age in Earth years to age in Venus years', function(){
+    let myAge = new Age(34);
+    expect(myAge.venusYears).toEqual('54.84');
+
+  })
+
+  it('should convert age in Earth years to age in Mars years', function(){
+    let myAge = new Age(34);
+    expect(myAge.marsYears).toEqual('18.09');
+
+  })
+
+  it('should convert age in Earth years to age in Jupiter years', function(){
+    let myAge = new Age(34);
+    expect(myAge.jupiterYears).toEqual('2.87');
+
+  })
+
 });
